75ce20d16935fd1b5b17b53759593d3d1212ff36
EvgeniyRybchuk <jeka.rubchuk@gmail.com>
2021-10-20 10:32:38 +0300

    make visual improvements

--- src/shared/services/converter.service.ts
+++ src/shared/services/converter.service.ts
@@ -9,13 +9,13 @@ import {HttpClient} from "@angular/common/http";
 export class ConverterService {
 
   public apiUrl = 'https://free.currconv.com/api/v7/convert'
-  public apiKey = '628ae22e24e06ca0cd00';
- // '54c2be3df7694299d28a'
+  //public apiKey = '628ae22e24e06ca0cd00';
+  public apiKey = '54c2be3df7694299d28a'
   constructor(private httpClient: HttpClient) { }
 
   public getCurrentRate(data: IExchangeRatesQuery): Observable<any>
   {
-    return this.httpClient.get<any>(`${this.apiUrl}?q=${data.to}_${data.from}&compact=ultra&apiKey=${this.apiKey}`);
+    return this.httpClient.get<any>(`${this.apiUrl}?q=${data.from}_${data.to}&compact=ultra&apiKey=${this.apiKey}`);
   }
 
 }
